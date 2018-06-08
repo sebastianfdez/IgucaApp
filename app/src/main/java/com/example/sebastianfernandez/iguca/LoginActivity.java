6d47013a720cebd49809786aba98b28d2edbdb87
Sebaastian <sfernandez1@uc.cl>
2018-06-08 00:58:59 -0400

    FEATURE: Input values, login activity

--- app/src/main/java/com/example/sebastianfernandez/iguca/LoginActivity.java
+++ app/src/main/java/com/example/sebastianfernandez/iguca/LoginActivity.java
@@ -1,29 +1,99 @@
 package com.example.sebastianfernandez.iguca;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button enterButton;
+    private TextView nameInput;
+    private TextView rutInput;
+    private TextView mailInput;
+
+    private Boolean nameInputEmpty = true;
+    private Boolean rutInputEmpty = true;
+    private Boolean mailInputEmpty = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        enterButton = findViewById(R.id.enterButton);
+        enterButton.setEnabled(false);
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("personName", nameInput.getText());
+                setResult(2, intent);
+                finish();//finishing activity
             }
         });
+
+        nameInput = findViewById(R.id.nameInput);
+        nameInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int aft ) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                nameInputEmpty = (nameInput.getText().length() == 0);
+                checkButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        rutInput = findViewById(R.id.rutInput);
+        rutInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int aft ) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                rutInputEmpty = (rutInput.getText().length() == 0);
+                checkButton();
+            }
+        });
+
+        mailInput = findViewById(R.id.mailInput);
+        mailInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int aft ) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mailInputEmpty = (mailInput.getText().length() == 0);
+                checkButton();
+            }
+        });
+    }
+
+    private void checkButton() {
+        Log.d("LOGIN3:", nameInputEmpty + "," + rutInputEmpty + "," + mailInputEmpty);
+        if (!nameInputEmpty && !rutInputEmpty && !mailInputEmpty) {
+            enterButton.setEnabled(true);
+        } else {
+            enterButton.setEnabled(false);
+        }
     }
 
 }
--- app/src/main/java/com/example/sebastianfernandez/iguca/MainActivity.java
+++ app/src/main/java/com/example/sebastianfernandez/iguca/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.sebastianfernandez.iguca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button enterButton;
-    private TextView nameInput;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +24,21 @@ public class MainActivity extends AppCompatActivity {
         // set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_main);
 
-        enterButton = (Button) findViewById(R.id.enterButton);
-        enterButton.setEnabled(true);
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(loginIntent, 2);
+    }
 
-            }
-        });
-
-        nameInput = (TextView) findViewById(R.id.nameInput);
-        nameInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.toString().trim().length()==0){
-                    enterButton.setEnabled(false);
-                } else {
-                    enterButton.setEnabled(true);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            String message = data.getStringExtra("personName");
+            textView = findViewById(R.id.textView);
+            textView.setText(message);
+        }
     }
 
 }
