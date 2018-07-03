cf72ff6f917264bfc50f6dfa069626d50fa062c3
Sebaastian <sfernandez1@uc.cl>
2018-07-02 20:29:51 -0400

    Feature: showing courses and saving files in cache

--- app/src/main/java/com/example/sebastianfernandez/iguca/AdminListCompanies.java
+++ app/src/main/java/com/example/sebastianfernandez/iguca/AdminListCompanies.java
@@ -1,6 +1,8 @@
 package com.example.sebastianfernandez.iguca;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -25,11 +27,9 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
 
 public class AdminListCompanies extends AppCompatActivity {
 
@@ -44,9 +44,14 @@ public class AdminListCompanies extends AppCompatActivity {
     private Button saveButton;
 
     private ArrayList <HashMap> companies;
+    private String[] companyNames;
+    private HashMap companySelected;
+    private HashMap companySelectedOriginal;
+
     private ArrayList <String> selectedCoursesIds;
     private boolean[] selectedCoursesCheck;
-    private HashMap companySelected;
+    private ArrayList selectedCompanyCourses;
+    private String[] selectedCompanyCoursesNames;
 
     private HashMap courses;
 
@@ -66,44 +71,44 @@ public class AdminListCompanies extends AppCompatActivity {
         this.getCourses ();
     }
 
+    public static boolean cleanDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = cleanDir (new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
+
     public void displayCompanies() {
-        companies = new ArrayList <HashMap> ( ( Collection ) (( HashMap ) dataSnapshot.child ( "Companies" ).getValue ()).values () );
-        courses = ((HashMap) dataSnapshot.child ( "Cursos" ).getValue ());
+
         selectedCoursesIds = new ArrayList <String> ();
 
-        final String[] companyNames = new String[companies.size ()];
-        Integer i = 0;
-        for ( HashMap company : companies ) {
-            companyNames[i] = ( String ) company.get ( "name" );
-            i++;
-        }
+        getCompanyNames ();
+
         uploadCourseNumber ();
 
-        ListItemAdaptor itemAdapter = new ListItemAdaptor( this, companyNames );
+        final ListItemAdaptor itemAdapter = new ListItemAdaptor( this, companyNames );
 
         coursesListView = (ListView) findViewById( R.id.coursesListView );
         coursesListView.setAdapter( itemAdapter );
 
         coursesListView.setOnItemClickListener ( new AdapterView.OnItemClickListener () {
             @Override
-            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
-                companySelected = companies.get ( position );
-                final Integer coursesListSize = (( HashMap ) companySelected.get ( "courses" )).size ();
-                final String[] coursesNames = new String[coursesListSize];
-                selectedCoursesCheck = new boolean[coursesListSize];
-                Integer i = 0;
-                for ( Map.Entry <String, String> courseId : (( HashMap <String, String> ) companySelected.get ( "courses" )).entrySet () ) {
-                    coursesNames[i] = courseId.getValue ();
-                    if (selectedCoursesIds.contains ( courseId.getKey () )) {
-                        selectedCoursesCheck[i] = true;
-                    } else {
-                        selectedCoursesCheck[i] = false;
-                    }
-                    i++;
+            public void onItemClick(AdapterView <?> parent, View view, final int position, long id) {
+                companySelected = companies.get(position);
+                if (companySelectedOriginal == null) {
+                    companySelectedOriginal = companySelected;
                 }
+                getCompanyCourses();
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder ( AdminListCompanies.this );
                 mBuilder.setTitle ( "Seleccionar Cursos de " + companyNames[position] );
-                mBuilder.setMultiChoiceItems ( coursesNames, selectedCoursesCheck, new DialogInterface.OnMultiChoiceClickListener () {
+                mBuilder.setMultiChoiceItems ( selectedCompanyCoursesNames, selectedCoursesCheck, new DialogInterface.OnMultiChoiceClickListener () {
                     @Override
                     public void onClick(DialogInterface dialog, int positionCourse, boolean isChecked) {
 
@@ -112,21 +117,36 @@ public class AdminListCompanies extends AppCompatActivity {
                 mBuilder.setPositiveButton ( "SELECCIONAR", new DialogInterface.OnClickListener () {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        Boolean courseSelected = false;
+                        if (companySelected != companySelectedOriginal) {
+                            selectedCoursesIds.clear ();
+                        }
                         for ( int i = 0; i < selectedCoursesCheck.length; i++ ) {
-                            String courseId = ( String ) (new ArrayList <> ( ( Collection ) (( HashMap ) companySelected.get ( "courses" )).keySet () )).get ( i );
+                            String courseKey = ( String ) selectedCompanyCourses.get ( i );
                             if (selectedCoursesCheck[i]) {
-                                if (!selectedCoursesIds.contains ( courseId )) {
-                                    selectedCoursesIds.add ( courseId );
+                                if (!selectedCoursesIds.contains ( courseKey )) {
+                                    selectedCoursesIds.add ( courseKey );
                                     saveButton.setEnabled ( true );
+                                    courseSelected = true;
                                 }
                             } else {
-                                if (selectedCoursesIds.contains ( courseId )) {
-                                    selectedCoursesIds.remove ( courseId );
+                                if (selectedCoursesIds.contains ( courseKey )) {
+                                    selectedCoursesIds.remove ( courseKey );
                                     saveButton.setEnabled ( true );
                                 }
                             }
-                            uploadCourseNumber ();
                         }
+                        companySelectedOriginal = companySelected;
+                        getCompanyNames ();
+                        if (selectedCoursesIds.size () > 0) {
+                            companyNames[position] = companyNames[position] + " ( " + selectedCoursesIds.size () + " )";
+                        }
+                        for (Integer j = 0; j < courses.size (); j++) {
+                            TextView nameTV = ((View) coursesListView.getChildAt ( j )).findViewById ( R.id.main_list_item_name );
+                            nameTV.setText ( companyNames[j] );
+
+                        }
+                        uploadCourseNumber ();
                     }
                 } );
                 mBuilder.setNegativeButton ( "CANCELAR", new DialogInterface.OnClickListener () {
@@ -141,6 +161,37 @@ public class AdminListCompanies extends AppCompatActivity {
         } );
     }
 
+    public void getCompanyCourses() {
+        selectedCompanyCourses = (new ArrayList<String> ((Collection)companySelected.get ( "courses" )));
+        final Integer coursesListSize = (selectedCompanyCourses).size ();
+        selectedCompanyCoursesNames = new String[coursesListSize];
+        selectedCoursesCheck = new boolean[coursesListSize];
+        Integer i = 0;
+        // Iguca id key
+        for (  Integer num = 0; num < selectedCompanyCourses.size (); num++ ) {
+            DataSnapshot course = dataSnapshot.child ( "Cursos" ).child ( ( String ) selectedCompanyCourses.get ( num ) );
+            selectedCompanyCoursesNames[num] = ( String ) ((HashMap)course.getValue ()).get ( "name" );
+            if (selectedCoursesIds.contains (course.getKey ()) && (companySelected.get ( "_id" ) == companySelectedOriginal.get ( "_id" ))) {
+                selectedCoursesCheck[num] = true;
+            } else {
+                selectedCoursesCheck[num] = false;
+            }
+        }
+
+    }
+
+    public void getCompanyNames() {
+        companies = new ArrayList <HashMap> ( ( Collection ) (( HashMap ) dataSnapshot.child ( "Companies" ).getValue ()).values () );
+        courses = ((HashMap) dataSnapshot.child ( "Cursos" ).getValue ());
+
+        companyNames = new String[companies.size ()];
+        Integer i = 0;
+        for ( HashMap company : companies ) {
+            companyNames[i] = ( String ) company.get ( "name" );
+            i++;
+        }
+    }
+
     public void enableSaveButton() {
 
         saveButton = findViewById ( R.id.saveButton );
@@ -148,27 +199,60 @@ public class AdminListCompanies extends AppCompatActivity {
         saveButton.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
+                cleanDir ( (getBaseContext ()).getCacheDir () );
+
+                File[] filesDirectories = new File[4];
+                filesDirectories[0] = new File(getBaseContext ().getCacheDir (), "Manual");
+                filesDirectories[0].mkdir ();
+                filesDirectories[1] = new File(getBaseContext ().getCacheDir (), "Ejercicios");
+                filesDirectories[1].mkdir ();
+                filesDirectories[2] = new File(getBaseContext ().getCacheDir (), "Examen");
+                filesDirectories[2].mkdir ();
+                filesDirectories[3] = new File(getBaseContext ().getCacheDir (), "Respuestas");
+                filesDirectories[3].mkdir ();
+
+                final Integer[] succesExpect = {selectedCoursesIds.size () * 4};
+                final Integer[] succesCount = {0};
+
+                new File(getBaseContext ().getCacheDir (), "Manual");
                 for ( Integer i = 0; i < selectedCoursesIds.size (); i++ ) {
-                    HashMap newCourse = ( HashMap ) ((HashMap) dataSnapshot.child ( "Cursos" ).getValue ()).get ( selectedCoursesIds.get ( i ) );
-                    StorageReference manualRef = (StorageReference) (storage.getReference ().child ( "" + newCourse.get ( "name" ) + "/Manual" ));
-                    try {
-                        File localFile = File.createTempFile("" + newCourse.get ( "_id" ) + "Manual", ".pdf");
-                        manualRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot> () {
+                    String newCourseKey = ( String  ) dataSnapshot.child ( "Cursos" ).child ( selectedCoursesIds.get ( i ) ).getKey ();
+
+                    StorageReference[] StorageRefs = new StorageReference[4];
+
+                    StorageRefs[0] = (StorageReference) (storage.getReference ().child ( newCourseKey + "/Manual" ));
+                    StorageRefs[1] = (StorageReference) (storage.getReference ().child ( newCourseKey + "/Ejercicios" ));
+                    StorageRefs[2] = (StorageReference) (storage.getReference ().child ( newCourseKey + "/Examen" ));
+                    StorageRefs[3] = (StorageReference) (storage.getReference ().child ( newCourseKey + "/Respuestas" ));
+
+                    for (Integer fileNum = 0; fileNum < 4; fileNum++) {
+
+                        //File localFile = File.createTempFile(newCourseKey, ".pdf", filesDirectories[fileNum]);
+                        File localFile = new File(filesDirectories[fileNum], newCourseKey + ".pdf");
+                        StorageRefs[fileNum].getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot> () {
                             @Override
                             public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                                 // Local temp file has been created
-                                Log.d ( "herma", "agua" );
+                                succesCount[0]++;
+                                if (succesCount[0] == succesExpect[0]) {
+                                    Intent secretLoginIntent = new Intent();
+                                    secretLoginIntent.putExtra("result", selectedCoursesIds);
+                                    if (getParent() == null) {
+                                        setResult(Activity.RESULT_OK, secretLoginIntent);
+                                    }
+                                    else {
+                                        getParent().setResult(Activity.RESULT_OK, secretLoginIntent);
+                                    }
+                                    finish();
+                                }
                             }
                         }).addOnFailureListener(new OnFailureListener () {
                             @Override
                             public void onFailure(@NonNull Exception exception) {
                                 // Handle any errors
-                                Log.d ( "herma", "agua" );
+                                Log.d ( "!" , "H");
                             }
                         });
-                    } catch (IOException e) {
-                        Log.d ( "herma", "agua" );
-                        e.printStackTrace ();
                     }
                 }
             }
--- app/src/main/java/com/example/sebastianfernandez/iguca/MainActivity.java
+++ app/src/main/java/com/example/sebastianfernandez/iguca/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -38,32 +39,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult( requestCode, resultCode, data );
-        // check if the request code is same as what is passed  here it is 2
-
-        switch (requestCode) {
-            case (2): {
-                if (resultCode == Activity.RESULT_OK) {
-                    String message = data.getStringExtra( "personName" );
-                    textView = findViewById( R.id.textView );
-                    textView.setText( message );
-                }
-                break;
-            }
-        }
-    }
-
     private void createSecretLogin() {
         mySecretLogin = (ImageView) findViewById( R.id.secretLoginIV );
         mySecretLogin.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent showSecretLoginActivity = new Intent( getApplicationContext(), SecretLogin.class );
-                startActivity( showSecretLoginActivity );
+                startActivityForResult ( showSecretLoginActivity, 3);
             }
         } );
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+
+        Log.d ( "h", "l" );
+
+        if (requestCode == 3) {
+            if(resultCode == Activity.RESULT_OK){
+                String result = data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
     }
 
     private void makeMainList() {
